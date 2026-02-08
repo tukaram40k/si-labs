ebf1cb319a388eea33a736daf8559fc5712fabe2
tukaram40k <tukaram40k@gmail.com>
2026-02-08 18:43:15 +0200

    setup environment for lab 1

--- lab1/src/main.cpp
+++ lab1/src/main.cpp
@@ -1,31 +1,18 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-#include "Arduino.h"
+#include <Arduino.h>
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-// #define LED_BUILTIN 13
+// put function declarations here:
+int myFunction(int, int);
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+void loop() {
+  // put your main code here, to run repeatedly:
+}
 
-  // wait for a second
-  delay(1000);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(1000);
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
 }
