961408217a0d09932706771694e1eb18c9bd62e7
tukaram40k <tukaram40k@gmail.com>
2026-02-16 16:29:09 +0200

    post-submission changes

--- lab-1-2/src/main.cpp
+++ lab-1-2/src/main.cpp
@@ -18,6 +18,10 @@ KeypadController keypad(7, 6, 5, 4, 3, 2, 1, 0);
 
 // current code being entered
 String codeBuffer = "";
+String secretCode = "3344";
+
+bool programmingMode = false;
+bool accessGranted = false;
 
 void setup() {
   // init led controllers
@@ -39,46 +43,27 @@ void setup() {
 void loop() {
   // check for key presses using stdio
   int key = getchar();
-  
+
   if (key != EOF) {
     char ch = (char)key;
-    if (ch != '*') {
-      codeBuffer += ch;
-
-      // update lcd
-      lcd.clear();
-      printf("%s\n", codeBuffer.c_str());
-
-      // match the code
-      if (codeBuffer == "3344") {
-        lcd.clear();
-        printf("Correct code!\n");
-        green_led.turnOn();
-        red_led.turnOff();
-
-        delay(2000);
-
+    
+    if (ch == '#') {
+      // allow changing the secret code if access is granted
+      if (accessGranted) {
+        programmingMode = true;
         codeBuffer = "";
-
         lcd.clear();
-        printf("Enter code:\n");
-      }
-      // check if 4 chars reached
-      else if (codeBuffer.length() >= 4) {
+        printf("Prog Mode:\n");
+      } else {
         lcd.clear();
-        printf("Incorrect!\n");
-        red_led.turnOn();
-        green_led.turnOff();
-
+        printf("Enter code first.\n");
         delay(2000);
-
-        codeBuffer = "";
-
+        
         lcd.clear();
-        printf("Enter code:\n");
+        printf("%s\n", codeBuffer.c_str());
       }
-    }
-    else {
+    } 
+    else if (ch == '*') {
       codeBuffer = "";
       lcd.clear();
       printf("Code cleared\n");
@@ -86,6 +71,67 @@ void loop() {
 
       lcd.clear();
       printf("Enter Code:\n");
+    }
+    else {
+      codeBuffer += ch;
+
+      // update lcd
+      lcd.clear();
+      
+      if (programmingMode) {
+        printf("Prog: %s\n", codeBuffer.c_str());
+      } else {
+        printf("%s\n", codeBuffer.c_str());
+      }
+
+      // check if in programming mode
+      if (programmingMode) {
+        if (codeBuffer.length() >= 4) {
+          // set the new secret code
+          secretCode = codeBuffer.substring(0, 4);
+          programmingMode = false;
+          codeBuffer = "";
+          
+          lcd.clear();
+          printf("New code set!\n");
+          delay(1000);
+          
+          lcd.clear();
+          printf("Enter Code:\n");
+        }
+      } 
+      else {
+        // match the code
+        if (codeBuffer == secretCode) {
+          lcd.clear();
+          printf("Correct code!\n");
+          green_led.turnOn();
+          red_led.turnOff();
+          accessGranted = true;
+
+          delay(2000);
+
+          codeBuffer = "";
+
+          lcd.clear();
+          printf("Enter code:\n");
+        }
+        // check if 4 chars reached
+        else if (codeBuffer.length() >= 4) {
+          lcd.clear();
+          printf("Incorrect!\n");
+          red_led.turnOn();
+          green_led.turnOff();
+          accessGranted = false;
+
+          delay(2000);
+
+          codeBuffer = "";
+
+          lcd.clear();
+          printf("Enter code:\n");
+        }
+      }
     }
   }
 }
