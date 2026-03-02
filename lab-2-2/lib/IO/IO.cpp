0a26e6e50abfaf111399b8267c17a9a91727eef9
tukaram40k <tukaram40k@gmail.com>
2026-03-02 11:50:34 +0200

    transition to esp32

--- lab-2-2/lib/IO/IO.cpp
+++ lab-2-2/lib/IO/IO.cpp
@@ -1,30 +1,24 @@
 #include "IO.h"
-
-static FILE serial_stream;
+#include <cstdarg>
 
 namespace IO {
-  int serial_putchar(char c, FILE *stream) {
-    Serial.write(c);
-    if (c == '\n') {
-      Serial.write('\r');
-    }
-    return 0;
+  int printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    char buffer[256];
+    int len = vsnprintf(buffer, sizeof(buffer), format, args);
+    va_end(args);
+    Serial.print(buffer);
+    return len;
   }
 
-  int serial_getchar(FILE *stream) {
-    while (!Serial.available());
-    char c = Serial.read();
-    Serial.write(c);
-    return c;
-  }
-  
   void setup() {
     // start serial
     Serial.begin(9600);
-  
-    // redirect std to serial
-    fdev_setup_stream(&serial_stream, serial_putchar, serial_getchar, _FDEV_SETUP_RW);
-    stdout = &serial_stream;
-    stdin = &serial_stream;
+    
+    // wait for serial port to connect
+    while (!Serial) {
+      ; // wait
+    }
   }
 }
