9d1d489b1b6d8bb5a4cbf487b2a17c6e54b6f1d3
tukaram40k <tukaram40k@gmail.com>
2026-03-03 15:50:45 +0200

    format files properly

--- lab-2-2/src/config.h
+++ lab-2-2/src/config.h
@@ -1,20 +1,20 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// GPIO Pin Definitions (per diagram.json)
+// pin definitions
 #define BUTTON_PIN      14
 #define GREEN_LED_PIN   16
 #define RED_LED_PIN     17
 #define YELLOW_LED_PIN  18
 
-// Timing Configuration
+// timing config
 #define SHORT_PRESS_THRESHOLD_MS    500
 #define TASK_MEASURE_PERIOD_MS      10
 #define YELLOW_BLINK_DELAY_MS       100
 #define INDICATION_DURATION_MS      150
 #define TASK_REPORT_PERIOD_MS       10000
 
-// Task Configuration
+// task config
 #define TASK_MEASURE_STACK_SIZE     8192
 #define TASK_MEASURE_PRIORITY       2
 
@@ -24,4 +24,4 @@
 #define TASK_REPORT_STACK_SIZE      8192
 #define TASK_REPORT_PRIORITY        2
 
-#endif // CONFIG_H
+#endif
--- lab-2-2/src/main.cpp
+++ lab-2-2/src/main.cpp
@@ -2,36 +2,31 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/semphr.h"
-
 #include "config.h"
-#include "task_measure.h"
-#include "task_stats.h"
-#include "task_report.h"
+#include "tasks/task_measure.h"
+#include "tasks/task_stats.h"
+#include "tasks/task_report.h"
 
-// Semaphore and mutex handles
+// semaphore and mutex handles
 static SemaphoreHandle_t s_xPressSemaphore = nullptr;
 static SemaphoreHandle_t s_xStatsMutex = nullptr;
 
 void setup() {
-    // Create binary semaphore for press event signaling (Task 1 -> Task 2)
-    s_xPressSemaphore = xSemaphoreCreateBinary();
-    
-    // Create mutex for protecting shared statistics data
-    s_xStatsMutex = xSemaphoreCreateMutex();
-    
-    // Create Task 1: Button Detection & Duration Measurement
-    vTaskMeasureCreate(s_xPressSemaphore, s_xStatsMutex);
-    
-    // Create Task 2: Statistics & Yellow LED Blink
-    vTaskStatsCreate(s_xPressSemaphore, s_xStatsMutex);
-    
-    // Create Task 3: Periodic Reporting
-    vTaskReportCreate(s_xStatsMutex);
-    
-    // Start the scheduler - this will never return
-    vTaskStartScheduler();
+  // semaphore for button press event
+  s_xPressSemaphore = xSemaphoreCreateBinary();
+
+  // mutex for stats
+  s_xStatsMutex = xSemaphoreCreateMutex();
+
+  // tasks
+  vTaskMeasureCreate(s_xPressSemaphore, s_xStatsMutex);
+  vTaskStatsCreate(s_xPressSemaphore, s_xStatsMutex);
+  vTaskReportCreate(s_xStatsMutex);
+
+  // start scheduler
+  vTaskStartScheduler();
 }
 
 void loop() {
-    // This will never be called
+
 }
