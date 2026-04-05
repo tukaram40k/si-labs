db144a142e840b4260c9b6f7161cc5a11c1c01ac
tukaram40k <tukaram40k@gmail.com>
2026-04-05 19:33:05 +0300

    implement lab 5-1

--- lab-5-1/src/config.h
+++ lab-5-1/src/config.h
@@ -4,8 +4,9 @@
 #include "tasks/task_conditioning.h"
 #include "tasks/task_reporting.h"
 
-namespace Config {
-  constexpr uint8_t RELAY_PIN = 7;
+namespace Config
+{
+  constexpr uint8_t RELAY_PIN = 12;
   constexpr uint8_t DHT11_PIN = 3;
   constexpr uint8_t BTN_UP_PIN = 9;
   constexpr uint8_t BTN_DOWN_PIN = 8;
@@ -16,28 +17,23 @@ namespace Config {
       .downButtonPin = BTN_DOWN_PIN,
       .buttonsActiveLow = true,
       .buttonDebounceMs = 30,
-      .setPointInitPct = 55,
+      .setPointInitPct = 45,
       .setPointMinPct = 30,
       .setPointMaxPct = 90,
       .setPointStepPct = 1,
-      .hysteresisPct = 3.0f,
-      // Fast button response with debouncing.
+      .hysteresisPct = 2.0f,
       .buttonPeriodMs = 30,
-    // DHT11 minimum read interval is about 2 s.
-    .samplePeriodMs = 2000,
-      // Control check can be faster than sensor sampling.
+      .samplePeriodMs = 2000,
       .controlPeriodMs = 100,
   };
 
   inline constexpr TaskActuatorControl::Config ACTUATOR_CFG{
       .relayPin = RELAY_PIN,
       .relayActiveHigh = true,
-      // Relay command update period.
       .periodMs = 50,
   };
 
   inline constexpr TaskReporting::Config REPORTING_CFG{
-      // Human-readable periodic report over STDIO.
       .periodMs = 1000,
   };
 }
